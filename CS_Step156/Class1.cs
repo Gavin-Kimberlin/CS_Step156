8f63a9d3e2edde72db2052f5567c8ed648496818
Gavin Kimberlin <guviner@gmail.com>
2020-06-16 01:52:13 -0700

    Create a program that takes in 2 inputs with the second one being optional and perform math operations.

---
+++ CS_Step156/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Step156
+{
+    class Class1
+    {
+        public static int Method(int input1, int input2 = 0)
+        {
+            int output = (input1 + input2) * 4;
+            return output;
+        }
+
+    }
+}
--- CS_Step156/Program.cs
+++ CS_Step156/Program.cs
@@ -10,6 +10,25 @@ namespace CS_Step156
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n                                                        Math App 3\n=======================================================================================================================");
+            Console.WriteLine("Enter a number:");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number if you'd like or press enter:");
+            string userNum2 = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(userNum2))
+            {
+                int result = Class1.Method(userNum1);
+                Console.WriteLine(userNum1 + " x 4 = " + result);
+            }
+            else
+            {
+                int convertInput2 = Convert.ToInt32(userNum2);
+                int result = Class1.Method(userNum1, convertInput2);
+                Console.WriteLine("(" + userNum1 + " + " + convertInput2 + ") x 4 = " + result);
+            }
+
+            Console.ReadLine();
         }
     }
 }
